8253e5bf1d1606695c97d083f104f56da4037e49
mecaota <b1014167@fun.ac.jp>
2017-05-30 00:39:10 +0900

    ヒストグラム自動生成プログラム完成
    imageフォルダ内をすべて自動読み込みしてすべてヒストグラムとグレー画像をそれぞれ生成できるようになった。グラフの横軸幅指定がまだ

--- histgram.py
+++ histgram.py
@@ -8,15 +8,20 @@ import numpy as np
 if __name__ == '__main__':
     dir_list = os.listdir('image')
     for i in dir_list:
-        ###グレー画像インポート、保存
-        image = cv2.imread(i, cv2.IMREAD_GRAYSCALE) #image import
+        ###グレー画像読み込み###
+        filename = "image/" + i
+        image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) #image import
+        print(filename)
+
+        ###グレー画像を保存###
         filename = "gray_image/gray_" + i
         cv2.imwrite(filename, image)
         print("image list")
-        print(i)
+        print(filename)
 
-        ###ヒストグラフ化、グラフ処理、保存
+        ###ヒストグラフ化、グラフ処理、保存###
         filename = "graph/histgram_" + i
+        print(filename)
         hist = cv2.calcHist([image], [0], None, [256], [0,256])
         pyplot.plot(hist)
         pyplot.title(i+" histgram")
